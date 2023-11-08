364f2f0ca8201ba305062a200656aa8e21eeeb7c
Rafael <rafa.viana.moc@gmail.com>
2023-11-08 12:09:48 -0300

    first commit

---
+++ app/src/main/java/com/rafa/travelapp/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,17 @@
+package com.rafa.travelapp.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rafa.travelapp.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
