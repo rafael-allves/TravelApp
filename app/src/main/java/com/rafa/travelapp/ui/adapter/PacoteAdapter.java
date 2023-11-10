ee2337f285441e23c287e91b9259fd043e7305a2
Rafael <rafa.viana.moc@gmail.com>
2023-11-10 10:50:22 -0300

    parser done

--- app/src/main/java/com/rafa/travelapp/ui/adapter/PacoteAdapter.java
+++ app/src/main/java/com/rafa/travelapp/ui/adapter/PacoteAdapter.java
@@ -1,8 +1,6 @@
 package com.rafa.travelapp.ui.adapter;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +8,11 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.core.content.ContextCompat;
-
 import com.rafa.travelapp.R;
 import com.rafa.travelapp.model.PacoteModel;
+import com.rafa.travelapp.ui.dataPass.PacoteDataPass;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.List;
-import java.util.Locale;
 
 
 public class PacoteAdapter extends BaseAdapter {
@@ -49,20 +43,16 @@ public class PacoteAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        PacoteModel data = pacotes.get(i);
-
         View pacote = LayoutInflater.from(context)
                 .inflate(R.layout.activity_lista_pacotes_item_pacote, viewGroup, false);
 
         ImageView imageView = pacote.findViewById(R.id.item_pacote_image);
         TextView nomeLocal = pacote.findViewById(R.id.item_pacote_nome_local);
         TextView dias = pacote.findViewById(R.id.item_pacote_dias);
-
-
         TextView preco = pacote.findViewById(R.id.item_pacote_preco);
-        NumberFormat brazilianFormat = DecimalFormat
-                .getCurrencyInstance(new Locale("pt", "br"));
-        preco.setText(brazilianFormat.format(data.getPreco()));
+
+        new PacoteDataPass(context, pacotes.get(i))
+                .bindContent(imageView, nomeLocal, dias, preco);
 
         return pacote;
     }
--- app/src/main/java/com/rafa/travelapp/ui/dataPass/PacoteDataPass.java
+++ app/src/main/java/com/rafa/travelapp/ui/dataPass/PacoteDataPass.java
@@ -8,50 +8,58 @@ import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
 
-import com.rafa.travelapp.R;
 import com.rafa.travelapp.model.PacoteModel;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class PacoteDataPass {
     private final Context context;
-    private final int imageViewId;
-    private final int nomeLocalId;
-    private final int diasId;
-    private final int precoId;
     private final PacoteModel pacote;
 
-    public PacoteDataPass(Context context, PacoteModel pacote, int imageViewId, int nomeLocalId, int diasId, int precoId) {
+    public PacoteDataPass(Context context, PacoteModel pacote) {
         this.context = context;
-        this.imageViewId = imageViewId;
-        this.nomeLocalId = nomeLocalId;
-        this.diasId = diasId;
-        this.precoId = precoId;
         this.pacote = pacote;
     }
 
-    public void passData(ImageView imagePacote, TextView nomeLocal, TextView dias,
-    TextView pacoteData, TextView preco)
-    {
+    public void bindContent(ImageView imagePacote, TextView nomeLocal, TextView dias,
+                         TextView pacoteData, TextView preco) {
         passImage(imagePacote);
+        passNomeLocal(nomeLocal);
+        passDias(dias);
+
+        passPreco(preco);
     }
 
-    private void passImage(ImageView imageView)
-    {
+    public void bindContent(ImageView imagePacote, TextView nomeLocal, TextView dias, TextView preco) {
+        passImage(imagePacote);
+        passNomeLocal(nomeLocal);
+        passDias(dias);
+        passPreco(preco);
+    }
+
+    private void passImage(ImageView imageView) {
         Resources resources = context.getResources();
         int imageId = resources.getIdentifier(pacote.getImageSource(), "drawable", context.getPackageName());
         Drawable image = ContextCompat.getDrawable(context, imageId);
         imageView.setImageDrawable(image);
     }
 
-    private void passNomeLocal(TextView nomeLocal)
-    {
+    private void passNomeLocal(TextView nomeLocal) {
         nomeLocal.setText(pacote.getNomeLocal());
     }
 
-    private void passDias(TextView dias)
-    {
+    private void passDias(TextView dias) {
         dias.setText("" + pacote.getDias() + " " + (
                         pacote.getDias() > 1 ? "Dias" : "Dia"
                 )
         );
+    }
+
+    private void passPreco(TextView preco) {
+        NumberFormat brazilianFormat = DecimalFormat
+                .getCurrencyInstance(new Locale("pt", "br"));
+        preco.setText(brazilianFormat.format(pacote.getPreco()));
     }
 }
